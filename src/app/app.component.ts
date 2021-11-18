d1d5ed83032da314353058bed0bacbb4ecd2e76c
sarath809605 <sarathkumar.merangi@gmail.com>
2021-11-19 00:54:42 +0530

    new change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myportfolio';
+
+  ngOnInit():void{
+
+  }
+
+  onAboutClick(): any{
+    var element = document.getElementById("about");
+    element.scrollIntoView({behavior: "smooth", block: "start"});
+  }
+  onWorkClick(): any{
+    var element = document.getElementById("work");
+    element.scrollIntoView({behavior: "smooth", block: "start"});
+  }
+  onAddressClick(): any{
+    var element = document.getElementById("address");
+    element.scrollIntoView({behavior: "smooth", block: "start"});
+  }
+  onContactClick(): any{
+    var element = document.getElementById("contact");
+    element.scrollIntoView({behavior: "smooth", block: "start"});
+  }
 }
