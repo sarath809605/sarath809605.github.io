68fd69d0e1f755b90594bee173b9a3cca7c73fed
sarath809605 <sarathkumar.merangi@gmail.com>
2021-11-20 00:21:23 +0530

    changes for mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +8,49 @@ import { Component,OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'myportfolio';
 
-  ngOnInit():void{
-
+  ngOnInit(): void {
+    this.fnAutoType();
   }
 
-  onAboutClick(): any{
+  fnAutoType() {
+    var i = 0;
+    var txt = "i there I am Sarath.";
+    var speed = 100;
+    function typeWriter() {
+      if (i < txt.length) {
+        document.getElementById("idAutoText").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+      else {
+        var j = txt.length;
+        while (j) {
+          var curString = document.getElementById("idAutoText").innerHTML;
+          curString = curString.slice(0, -1);
+          document.getElementById("idAutoText").innerHTML = curString;
+          j--;
+        }
+        i = 0;
+        setTimeout(typeWriter, speed);
+      }
+    }
+    typeWriter();
+  }
+
+  onAboutClick(): any {
     var element = document.getElementById("about");
-    element.scrollIntoView({behavior: "smooth", block: "start"});
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
   }
-  onWorkClick(): any{
+  onWorkClick(): any {
     var element = document.getElementById("work");
-    element.scrollIntoView({behavior: "smooth", block: "start"});
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
   }
-  onAddressClick(): any{
+  onAddressClick(): any {
     var element = document.getElementById("address");
-    element.scrollIntoView({behavior: "smooth", block: "start"});
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
   }
-  onContactClick(): any{
+  onContactClick(): any {
     var element = document.getElementById("contact");
-    element.scrollIntoView({behavior: "smooth", block: "start"});
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 }
